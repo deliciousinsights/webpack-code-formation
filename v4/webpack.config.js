701b0f9a489b9753303ed51cbd19e55878e88027
Christophe Porteneuve <tdd@tddsworld.com>
2018-02-15 11:01:29 +0100

    JS : sourcemaps de dev

--- v4/webpack.config.js
+++ v4/webpack.config.js
@@ -1,11 +1,6 @@
-const Path = require('path')
-
 module.exports = {
-  entry: './src',
   output: {
     devtoolModuleFilenameTemplate: 'webpack:///[resource-path]',
-    filename: 'main.js',
-    path: Path.resolve(__dirname, './dist'),
   },
   devtool: 'cheap-module-source-map',
 }
