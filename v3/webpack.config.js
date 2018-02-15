6559ec1846973f21cc6e6da59e531395e1e6f0f6
Christophe Porteneuve <tdd@tddsworld.com>
2018-02-15 13:42:34 +0100

    JS : Babel

--- v3/webpack.config.js
+++ v3/webpack.config.js
@@ -1,11 +1,35 @@
 const Path = require('path')
 
+const PATHS = {
+  build: Path.resolve(__dirname, 'dist'),
+  source: Path.resolve(__dirname, 'src'),
+}
+
 module.exports = {
-  entry: './src',
+  entry: [PATHS.source],
   output: {
     devtoolModuleFilenameTemplate: 'webpack:///[resource-path]',
     filename: 'main.js',
-    path: Path.resolve(__dirname, './dist'),
+    path: PATHS.build,
   },
   devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: PATHS.source,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { modules: false, useBuiltIns: 'usage' }],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
--- v4/webpack.config.js
+++ v4/webpack.config.js
@@ -1,6 +1,38 @@
+const Path = require('path')
+
+const PATHS = {
+  build: Path.resolve(__dirname, 'dist'),
+  source: Path.resolve(__dirname, 'src'),
+}
+
 module.exports = {
+  entry: [PATHS.source],
   output: {
     devtoolModuleFilenameTemplate: 'webpack:///[resource-path]',
+    filename: 'main.js',
+    path: PATHS.build,
   },
   devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: PATHS.source,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { corejs: 3, modules: false, useBuiltIns: 'usage' },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
