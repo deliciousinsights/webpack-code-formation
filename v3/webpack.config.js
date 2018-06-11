84bd7140f2090c478371f96c089f9dfacf36c898
Christophe Porteneuve <tdd@tddsworld.com>
2018-06-11 16:00:58 +0200

    CSS : SASS (ou LESS, ou Stylus…)

--- v3/webpack.config.js
+++ v3/webpack.config.js
@@ -38,6 +38,14 @@ module.exports = {
           { loader: 'css-loader', options: { sourceMap: true } },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader', options: { sourceMap: true } },
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
     ],
   },
 }
--- v4/webpack.config.js
+++ v4/webpack.config.js
@@ -41,6 +41,14 @@ module.exports = {
           { loader: 'css-loader', options: { sourceMap: true } },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
     ],
   },
 }
